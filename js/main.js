cd739f53b802186e5c2a329be19c4895e596f719
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-06-08 10:49:37 -0500

    logic to draw right side blocks added and working

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ var blockColumnCount = 4;
 var blockWidth = 50;
 //height of each block
 var blockHeight = 50;
+//offset variables for drawing right blocks at desired position
+var blockOffsetTop = 300;
+var blockOffsetRight = 50;
 
 
 //create right side blocks 
@@ -23,7 +26,7 @@ for(c=0; c<blockColumnCount; c++){
 	for(r=0; r < blockRowCount; r++){
 
 		blocksRight[c][r] = {x: 0, y: 0};
-		console.log(blocksRight[c][r].x, ",", blocksRight[c][r].y);
+		//console.log(blocksRight[c][r].x, ",", blocksRight[c][r].y);
 		
 	}
 
@@ -38,10 +41,35 @@ for(c=0; c<blockColumnCount; c++){
 	for(r=0; r < blockRowCount; r++){
 
 		blocksLeft[c][r] = {x: 0, y: 0};
-		console.log(blocksLeft[c][r].x, ",", blocksLeft[c][r].y);
+		//console.log(blocksLeft[c][r].x, ",", blocksLeft[c][r].y);
 		
 	}
 
 }
 
+//create a function to draw the blocks on the page
+//loop through the blocks array and store the object location to detect collision later
+var drawBlocks = function(){
+
+
+	for(c=0; c<blockColumnCount; c++) {
+        for(r=0; r<blockRowCount; r++) {
+        	//create a variable to hold offsets for block x and y points
+        	var blockX = (c*(blockOffsetRight));
+            var blockY = (r*(blockOffsetTop)+100);
+
+            //add the created offset to blocks, it currently interchanges from top and bottom row points
+            blocksRight[c][r].x = blockX;
+            blocksRight[c][r].y = blockY;
+            console.log(blocksRight[c][r].x, ",", blocksRight[c][r].y);
+
+            ctx.beginPath();
+            ctx.rect(blockX, blockY, blockWidth, blockHeight);
+            ctx.stroke();
+
+        }
+    }
+}
+
+drawBlocks();
 
